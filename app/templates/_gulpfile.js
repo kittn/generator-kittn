bffcd93702dcabc6822248595b3a017462fc4f06
Sascha Fuchs <gisu2onic@gmail.com>
2015-11-29 15:14:05 +0100

    Restyled BrowserSync Notification
    Position is now bottom left, is not so annoying when working on Header Elements :)

--- app/templates/_gulpfile.js
+++ app/templates/_gulpfile.js
@@ -97,6 +97,22 @@ gulp.task('browser-sync', function() {
         links  : true,
         forms  : true }
     },
+      notify: {
+        styles: [
+          'padding: 10px 20px;',
+          'position: fixed;',
+          'font-size: 14px;',
+          'font-weight: bold',
+          'z-index: 9999;',
+          'top: inherit',
+          'border-radius: 0',
+          'right: 0;',
+          'bottom: 0;',
+          'color: #f4f8f9;',
+          'background-color: #026277;',
+          'text-transform: uppercase'
+        ]
+      },
       server: bsServer,
       open: kittn.browsersync.openbrowser
     });
