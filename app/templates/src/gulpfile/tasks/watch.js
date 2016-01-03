09dc528068f0214edb9a25f7ab188bb1ce979056
Sascha Fuchs <gisu2onic@gmail.com>
2016-01-03 13:40:00 +0100

    Added a full RebuildTask for Twig Files

--- app/templates/src/gulpfile/tasks/watch.js
+++ app/templates/src/gulpfile/tasks/watch.js
@@ -8,15 +8,18 @@ import watch from 'gulp-watch'
 import templateFiles from '../lib/templateFiles'
 import path from 'path'
 
-
 const watchTask = () => {
 
   // Watch the SCSS Folder for changes - compile CSS
-  gulp.watch([kc.src.style + '**/*.scss'], ['compiler:css']);
+  gulp.watch([kc.src.style + '**/*.scss'], ['compiler:css'])
 
   // Watch the Structure
-  gulp.watch([templateFiles()], ['compiler:html']);
+  gulp.watch([templateFiles()], ['compiler:html'])
 
+  <% if ( projectstructure == 'Twig Template' ) { %>// When a Template File is modified all Child rerendered
+  if(kc.template.compiler) {
+    gulp.watch([kc.src.template + 'templates/*.twig'], ['compiler:twigRebuild'])
+  }<% }%>
 }
 
 gulp.task('watch', ['browser-sync'], watchTask)
--- test/fine.js
+++ test/fine.js
@@ -1,1 +1,2 @@
 console.log('Everything is fine');
+
