af0cd37885aa6c516b44ac455502d0314baa2a91
Sascha Fuchs <gisu2onic@gmail.com>
2016-01-26 16:51:58 +0100

    Bugfix for HTML Compiler

--- app/templates/_compile-html.js
+++ app/templates/_compile-html.js
@@ -4,7 +4,7 @@
  * @description Compiles with Jade or Twig (the decision too has been taken in the packet generating), also copy only the Files.
  */
 
-import kc from '../config.json'
+import kc from '../../config.json'
 import pkg from '../../package.json'
 import gulp from 'gulp'
 import gutil from 'gulp-util'
@@ -32,7 +32,7 @@ const compilerHtmlTask = () => {
   // Twig Compiler
   if(kc.template.compiler) {
     // TWIG // Jade
-    return <% if ( projectstructure == 'Twig Template' ) { %>gulp.src(kc.src.template + '*.twig')<% } else if ( projectstructure == 'Jade Template' ) { %>gulp.src([kc.src.template + '*.jade',kc.src.template + '_*.jade'])<% } %><% if ( projectstructure == 'Twig Template' ) { %>
+    return <% if ( projectstructure == 'Twig Template' ) { %>gulp.src(kc.src.template + '*.twig')<% } else ( projectstructure == 'Jade Template' ) { %>gulp.src([kc.src.template + '*.jade',kc.src.template + '_*.jade'])<% } %><% if ( projectstructure == 'Twig Template' ) { %>
       .pipe(global.changedOverride === false ? $.changed(kc.dist.markup, {extension: '.html'}) : gutil.noop())<% } %>
       .pipe($.plumber())<% if ( projectstructure == 'Twig Template' ) { %>
       .pipe($.twig({ data: templateLocals }))<% } else if ( projectstructure == 'Jade Template' ) { %>
