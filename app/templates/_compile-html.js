4cb44e8b3f9533387cc85dc1e3856d8a1a407f6b
Sascha Fuchs <gisu2onic@gmail.com>
2016-02-03 17:41:07 +0100

    Bugfix Condition

--- app/templates/_compile-html.js
+++ app/templates/_compile-html.js
@@ -32,7 +32,7 @@ const compilerHtmlTask = () => {
   // Twig Compiler
   if(kc.template.compiler) {
     // TWIG // Jade
-    return <% if ( projectstructure == 'Twig Template' ) { %>gulp.src(kc.src.template + '*.twig')<% } else ( projectstructure == 'Jade Template' ) { %>gulp.src([kc.src.template + '*.jade',kc.src.template + '_*.jade'])<% } %><% if ( projectstructure == 'Twig Template' ) { %>
+    return <% if ( projectstructure == 'Twig Template' ) { %>gulp.src(kc.src.template + '*.twig')<% } if ( projectstructure == 'Jade Template' ) { %>gulp.src([kc.src.template + '*.jade',kc.src.template + '_*.jade'])<% } %><% if ( projectstructure == 'Twig Template' ) { %>
       .pipe(global.changedOverride === false ? $.changed(kc.dist.markup, {extension: '.html'}) : gutil.noop())<% } %>
       .pipe($.plumber())<% if ( projectstructure == 'Twig Template' ) { %>
       .pipe($.twig({ data: templateLocals }))<% } else if ( projectstructure == 'Jade Template' ) { %>
