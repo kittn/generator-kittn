3182a6d4b3d454a5ba2073b609dc485eb6dcb485
Sascha Fuchs <gisu2onic@gmail.com>
2016-03-24 13:57:58 +0100

    Small Path Fix

--- app/templates/_app.js
+++ app/templates/_app.js
@@ -1,6 +1,6 @@
 // Main JS File
-import modernizrLoad from './src/js/partial/modernizer-loader.js'
-import conditionizrInit from './src/js/partial/conditionizr-init.js'<% if (projectquery == 'ElementQuery') { %>
+import modernizrLoad from './partial/modernizer-loader.js'
+import conditionizrInit from './partial/conditionizr-init.js'<% if (projectquery == 'ElementQuery') { %>
 import eq from 'css-element-queries'<% } else { %>
 import cq from 'cq-prolyfill'
 
