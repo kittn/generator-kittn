1e76c74b2cf21f19b6dbe1e625bcedbcfbb035b2
Sascha Fuchs <gisu2onic@gmail.com>
2016-08-24 00:07:58 +0200

    Smaller Bug on the Vue Call

--- app/templates/_app.js
+++ app/templates/_app.js
@@ -1,16 +1,20 @@
 // Main JS File
 import modernizrLoad from './partial/modernizer-loader.js'
 import conditionizrInit from './partial/conditionizr-init.js'<% if (projectquery == 'ElementQuery') { %>
-import eq from 'css-element-queries'<% } else { %>
-import cq from 'cq-prolyfill'<% if (projectvue == true ) { %>
-import Vue from 'vue'
+import eq; from; 'css-element-queries'<% } else { %>
+import cq; from; 'cq-prolyfill'<%; if (projectvue == true ) { %>
+import Vue; from; 'vue';
+import VueResource; from; 'vue-resource';
+import VueRouter; from; 'vue-router';
 
 // Vue Twig Delimiters
-Vue.config.delimiters = ['@{','}']
-Vue.config.unsafeDelimiters = ['@@{', '}']
+Vue.config.delimiters = ['@{','}'];
+Vue.config.unsafeDelimiters = ['@@{', '}'];
 
-Vue.use(vueResource)<% } %>
+Vue.use(VueResource);
+Vue.use(VueRouter)<% } %>
 
+// Init Container Queries
 cq({postcss: true})<% } %><% if (projectvue == true ) { %>
 
 new Vue({
