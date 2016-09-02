1005596c92501d033fda81a9d768b667d9853007
Sascha Fuchs <gisu2onic@gmail.com>
2016-09-02 17:30:23 +0200

    Bullshit IDE added semicolons

--- app/templates/_app.js
+++ app/templates/_app.js
@@ -1,11 +1,11 @@
 // Main JS File
 import modernizrLoad from './partial/modernizer-loader.js'
 import conditionizrInit from './partial/conditionizr-init.js'<% if (projectquery == 'ElementQuery') { %>
-import eq; from; 'css-element-queries'<% } else { %>
-import cq; from; 'cq-prolyfill'<%; if (projectvue == true ) { %>
-import Vue; from; 'vue';
-import VueResource; from; 'vue-resource';
-import VueRouter; from; 'vue-router';
+import eq from 'css-element-queries'<% } else { %>
+import cq from 'cq-prolyfill'<%; if (projectvue == true ) { %>
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import VueRouter from 'vue-router'
 
 // Vue Twig Delimiters
 Vue.config.delimiters = ['@{','}'];
