1980a38bbf4b961d72dc9366c1c0e5b98228bd7e
Sascha Fuchs <gisu2onic@gmail.com>
2016-10-18 23:51:49 +0200

    Adding Spacer and PXtoREM Function direct in the PostCSS Compiler

--- app/templates/src/gulpfile/lib/prePostCssConf.js
+++ app/templates/src/gulpfile/lib/prePostCssConf.js
@@ -2,11 +2,25 @@ import kc from '../../config.json'
 
 const preCssConf = () => {
   return [
+    function(css) { // Spacer Function
+      css.walkDecls(function (decl) {
+        if (decl.value.match(/spacer/)) {
+          const SPACER = parseInt(kc.css.spacer, 10)
+          decl.value = SPACER * parseFloat(decl.value) + 'px'
+        }
+        if (decl.value.match(/pxrem/)) {
+          const BASE = parseInt(kc.css.baseFontSize, 10)
+          decl.value = parseFloat(decl.value) / BASE + 'rem'
+        }
+      })
+    },
     // CSS Level 4 Stuff
     require('postcss-custom-selectors'),
     require('postcss-custom-media'),
     require('postcss-pseudo-class-any-link'),
     require('postcss-custom-properties'),
+    require('postcss-calc'),
+    require('postcss-aspect-ratio'),
     require('postcss-easings'),
     // Include Assets
     require('postcss-assets')({
@@ -18,6 +32,7 @@ const preCssConf = () => {
       browsers: kc.css.prefix,
       cascade: false
     }),
+    require('postcss-svg-fragments'),
     // Include the LostGrid System
     require('lost')({}),
     // Include Size Short hands
