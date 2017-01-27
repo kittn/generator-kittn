f0c121c3bd46d483720c498e5b4efb036013cc93
Sascha Fuchs <gisu2onic@gmail.com>
2017-01-27 14:21:10 +0100

    Removed Browsersync script on WP Template Footer thanks @martin

--- app/templates/src/skeletons/wordpress/template/footer.php
+++ app/templates/src/skeletons/wordpress/template/footer.php
@@ -4,7 +4,7 @@
   </div>
 
   <?php wp_footer(); ?>
-  <!-- Javascripts -->
+
   <% if ( projectiecompatible == true ) { %>
   <!--[if lte IE 9]>
   <script src="<?php bloginfo('template_url'); ?>/assets/js/selectivizr.js"></script>
@@ -12,15 +12,5 @@
   <script src="<?php bloginfo('template_url'); ?>/assets/js/modernizr.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/assets/js/conditionizr.js"></script>
   <script src="<?php bloginfo('template_url'); ?>/assets/js/main.js"></script>
-
-  <?php
-  $host = $_SERVER['HTTP_HOST'];
-  if( $host == <% if (projectcredential) { %>"<%= credentialdomain %>"<% } else { %>"localhost"<% } %>) {
-    ?>
-    <script type='text/javascript' id="__bs_script__">//<![CDATA[
-      document.write("<script async src='//HOST:3000/browser-sync/browser-sync-client.js'><\/script>".replace("HOST", location.hostname));
-      //]]></script>
-  <?php } ?>
-
 </body>
 </html>
