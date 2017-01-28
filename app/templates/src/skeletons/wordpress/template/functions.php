a498eea190dbeeae54e62e14c120c5e2931a7716
Sascha Fuchs <gisu2onic@gmail.com>
2017-01-28 18:09:50 +0100

    Renamed Directorys for Wordpress Skeleton

--- app/templates/src/skeletons/wordpress/template/functions.php
+++ app/templates/src/skeletons/wordpress/template/functions.php
@@ -1,35 +1,35 @@
 <?php
 
   // ACF Options Page
-  require_once('functions/acfOptions.php');
+  require_once('_functions/acfOptions.php');
 
   // Add RSS Link in head
-  require_once( 'functions/addRssLink.php' );
+  require_once('_functions/addRssLink.php');
 
   // Custom Loader GIF CF7
-  require_once( 'functions/cf7CustomLoader.php' );
+  require_once('_functions/cf7CustomLoader.php');
 
   // Add HTML5 Support
-  require_once( 'functions/html5Support.php' );
+  require_once('_functions/html5Support.php');
 
   // Images Sizes
-  require_once( 'functions/imagesSizes.php' );
+  require_once('_functions/imagesSizes.php');
 
   // Remove jQuery Migrate and add jQuery Core
-  require_once( 'functions/removeAddjQuery.php' );
+  require_once('_functions/removeAddjQuery.php');
 
   // Remove Admin Bar
-  require_once( 'functions/removeAdminBar.php' );
+  require_once('_functions/removeAdminBar.php');
 
   // Remove WP Generator
-  require_once('functions/removeWPGenerator.php');
+  require_once('_functions/removeWPGenerator.php');
 
   // Post Thumbnail Support
-  require_once( 'functions/thumbnailSupport.php' );
+  require_once('_functions/thumbnailSupport.php');
 
   // Add Title Tag Support
-  require_once( 'functions/titleTagSupport.php' );
+  require_once('_functions/titleTagSupport.php');
 
   // Translation
-  // require_once( 'functions/translation.php' );
+  // require_once('functions/translation.php');
 ?>
