de372f46a8a28da5f824fe1f9fe3f75b0b31da01
Sascha Fuchs <gisu2onic@gmail.com>
2017-02-04 00:40:32 +0100

    Refine Wordpress Imageset, is now much Smarter

--- app/templates/src/skeletons/starterpack/wordpress/structure/templates/_functions/imagesSizes.php
+++ app/templates/src/skeletons/starterpack/wordpress/structure/templates/_functions/imagesSizes.php
@@ -2,43 +2,52 @@
 /**
   Image Sizes
   ===========
-  A Set of Image Sizes
-*/
+  Build a Set of different Imagesets
+ */
 
-// Images
+// Uncopped Image Sizes
+$UNCROPPED_SIZES = [
+  'large'   => '1400',
+  'wide'    => '1200',
+  'desktop' => '960',
+  'tablet'  => '768',
+  'medium'  => '480',
+  'small'   => '320',
+  'micro'   => '150'
+];
+
+// Cropped Image Sizes
+$CROPPED_SIZES = [
+  'large'  => '1200',
+  'medium' => '800',
+  'small'  => '400',
+  'mini'   => '100'
+];
+
+// Different Formats with different ratios (insert the Result of Ratio hight/width)
+$CROPPED_FORMAT = [
+  'square'    => '1',      // 1:1
+  'wide'      => '0.5625', // 16:9
+  'extrawide' => '0.4761', // 21:10
+  'rect'      => '0.75'    // 4:3
+];
+
+// Basic Images Crops
 add_image_size('mini', 100);
 add_image_size('small', 600);
 add_image_size('medium', 1000);
 add_image_size('large', 1280);
 
-add_image_size('rw_large',  1400, 9999);
-add_image_size('rw_wide',   1200, 9999);
-add_image_size('rw_desktop', 960, 9999);
-add_image_size('rw_tablet',  768, 9999);
-add_image_size('rw_medium',  480, 9999);
-add_image_size('rw_small',   320, 9999);
-add_image_size('rw_micro',   150, 9999);
+// Generate Uncropped Images
+foreach ($UNCROPPED_SIZES as $key => $value) {
+  add_image_size('rw_'.$key,  $value, 9999);
+}
 
-// Square Crops - needed for Gallery
-add_image_size('square_large', 1200, 1200, true);
-add_image_size('square_medium', 800, 800, true);
-add_image_size('square_small', 400, 400, true);
-add_image_size('square_mini', 100, 100, true);
-
-// Wide Crops (16:9) - needed for Gallery
-add_image_size('wide_large', 1200, 675, true);
-add_image_size('wide_medium', 800, 450, true);
-add_image_size('wide_small', 400, 225, true);
-add_image_size('wide_mini', 100, 56, true);
-
-// Wide Crops (21:10) - needed for Gallery
-add_image_size('extrawide_large', 1200, 514, true);
-add_image_size('extrawide_medium', 800, 343, true);
-add_image_size('extrawide_small', 400, 171, true);
-add_image_size('extrawide_mini', 100, 43, true);
-
-// Wide Crops (4:3) - needed for Gallery
-add_image_size('rect_large', 1200, 900, true);
-add_image_size('rect_medium', 800, 600, true);
-add_image_size('rect_small', 400, 300, true);
-add_image_size('rect_mini', 100, 75, true);
+// Generate Crop Sets
+foreach ($CROPPED_FORMAT as $type => $ratio) {
+  // Generate Crop Sizes
+  foreach ($CROPPED_SIZES as $key => $value) {
+    $height = $value * $ratio;
+    add_image_size($type.'_'.$key, $value, $height, true);
+  }
+}
