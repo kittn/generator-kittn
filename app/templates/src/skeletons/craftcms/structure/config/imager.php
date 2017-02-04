044f14650fb4be1bf9e70f9132a52383d6b88f8b
Sascha Fuchs <gisu2onic@gmail.com>
2017-02-04 19:52:36 +0100

    Added Imager Config File

---
+++ app/templates/src/skeletons/craftcms/structure/config/imager.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Configuration file for Imager
+ *
+ * Override this by placing a file named 'imager.php' inside your config folder and override variables as needed.
+ * Multi-environment settings work in this file the same way as in general.php or db.php
+ */
+return array(
+  'interlace' => true, // false, true ('line'), 'none', 'line', 'plane', 'partition'x
+  'instanceReuseEnabled' => false,
+  'allowUpscale' => false
+);
