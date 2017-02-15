3b639db1528bb5835c8bca31de1b18ea19318e03
Sascha Fuchs <gisu2onic@gmail.com>
2017-02-15 18:42:02 +0100

    Added Condition for deactivate Progressloader on local environment

--- app/templates/src/skeletons/starterpack/wordpress/structure/templates/header.php
+++ app/templates/src/skeletons/starterpack/wordpress/structure/templates/header.php
@@ -3,9 +3,11 @@
 <?php // Include Head ?>
 <?php include ( get_template_directory() . '/_parts/document-header.php' ); ?>
 <body <?php body_class(); ?>>
+  <?php if (WP_ENV != 'local') : ?>
   <div class="o-progressloader">
     <div class="o-progressloader__loader"></div>
   </div>
+  <?php endif; ?>
   <div class="o-layout__wrapper">
     <?php // Include Pageheader ?>
     <?php include ( get_template_directory() . '/_parts/page-header.php' ); ?>
