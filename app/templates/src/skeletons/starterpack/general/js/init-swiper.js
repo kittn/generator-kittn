59f33054e190a7f8176e6d7824a9d639a93f2122
Sascha Fuchs <gisu2onic@gmail.com>
2017-02-21 02:25:35 +0100

    Accordions and Tabs can now be used inside a Section Colum. Refined Image and Textslider with addtitional Options for Setup. Added Card Element. Textslider now use this Cars Element

--- app/templates/src/skeletons/starterpack/general/js/init-swiper.js
+++ app/templates/src/skeletons/starterpack/general/js/init-swiper.js
@@ -1,28 +1,54 @@
 import Swiper from 'swiper'
 
-// Image Slider Config
-const imageslider = new Swiper('.o-imageslider', {
+// Image Slider Object
+const imageSliders = Array.from(document.querySelectorAll('.o-imageslider'))
+const imageSlidersDefaults = {
   pagination: '.o-imageslider__pagination',
-  paginationClickable: true,
   nextButton: '.o-imageslider__button-next',
   prevButton: '.o-imageslider__button-prev',
   wrapperClass: 'o-imageslider__wrapper',
   slideClass: 'o-imageslider__slide',
+  paginationClickable: true,
   spaceBetween: 0,
   loop: true,
   autoplay: 20000,
   lazyLoading: true
-})
+}
 
-// Text Slider Config
-const textslider = new Swiper('.o-textslider', {
+// Generate Image Sliders
+if (imageSliders) {
+  imageSliders.forEach(imageSlider => {
+    const getSettings = imageSlider.getAttribute('data-imageslider')
+    const config = JSON.parse(getSettings)
+    const combineSettings = Object.assign(imageSlidersDefaults, config)
+
+    // Add a new Slider
+    const newImageSlider = new Swiper(imageSlider, combineSettings)
+  })
+}
+
+// Test Slider Object
+const textSliders = Array.from(document.querySelectorAll('.o-textslider'))
+const textSlidersDefaults = {
   pagination: '.o-textslider__pagination',
-  paginationClickable: true,
   nextButton: '.o-textslider__button-next',
   prevButton: '.o-textslider__button-prev',
   wrapperClass: 'o-textslider__wrapper',
   slideClass: 'o-textslider__slide',
+  paginationClickable: true,
   spaceBetween: 60,
   autoHeight: true,
   loop: true
-})
+}
+
+// Generate Text Sliders
+if (textSliders) {
+  textSliders.forEach(textSlider => {
+    const getSettings = textSlider.getAttribute('data-textslider')
+    const config = JSON.parse(getSettings)
+    const combineSettings = Object.assign(textSlidersDefaults, config)
+
+    // Add a new Slider
+    const newtextSlider = new Swiper(textSlider, combineSettings)
+  })
+}
