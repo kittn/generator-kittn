214bebb83b7788c4777034880e5a73da65494a50
Sascha Fuchs <gisu2onic@gmail.com>
2017-02-24 18:46:33 +0100

    JS Fix

--- app/templates/src/skeletons/starterpack/general/js/init-heighttransition.js
+++ app/templates/src/skeletons/starterpack/general/js/init-heighttransition.js
@@ -2,10 +2,10 @@ import transition from 'transition-to-from-auto'
 
 // Trigger for the Slideout Box
 if (document.querySelector('.o-slideoutbox__trigger') !== null) {
-  let items = document.querySelectorAll('.o-slideoutbox')
+  const items = document.querySelectorAll('.o-slideoutbox')
   for (let i = 0; i < items.length; i++) {
-    let item = items[i]
-    let trigger = item.querySelector('.o-slideoutbox__trigger')
+    const item = items[i]
+    const trigger = item.querySelector('.o-slideoutbox__trigger')
     trigger.addEventListener('click', function(el) {
       const classes = el.srcElement.classList
       const slideElement = el.srcElement.parentElement.parentElement.querySelector('.o-slideoutbox__body')
@@ -31,10 +31,10 @@ if (document.querySelector('.o-slideoutbox__trigger') !== null) {
 
 // Trigger for the Slideout Box
 if (document.querySelector('.o-accordion__trigger') !== null) {
-  let items = document.querySelectorAll('.o-accordion')
+  const items = document.querySelectorAll('.o-accordion')
   for (let i = 0; i < items.length; i++) {
-    let item = items[i]
-    let triggers = item.querySelectorAll('.o-accordion__trigger')
+    const item = items[i]
+    const triggers = item.querySelectorAll('.o-accordion__trigger')
     triggers.forEach(function(item) {
       item.addEventListener('click', function(el) {
         const classes = el.srcElement.classList
