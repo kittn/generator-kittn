e19307def72f4956dcb8b36f318779f65d3ae1b8
Sascha Fuchs <gisu2onic@gmail.com>
2017-03-10 15:15:56 +0100

    Fuck Storm Code Cleanup!

--- app/templates/src/js/main.js
+++ app/templates/src/js/main.js
@@ -1,33 +1,33 @@
 // Main JS File<% if (projectjquery === true) { %>
 import $ from 'jquery'<% } %>
 import eq from 'eqcss'<% if ( projectJSFramework === 'Vue.js' ) { %>
-import Vue; from; 'vue';
-import { sync } from; 'vuex-router-sync';
-import axios; from; 'axios';
-import VueAxios; from; 'vue-axios';
-import router; from; './router/router';
-import store; from; './store/store'<% } %><% if (projectcraftbp === true || projectwordpressbp === true) { %>
-import progressLoader; from; './partial/progressloader';
-import initPhotoSwipeFromDOM; from; './partial/init-photoswipe' // eslint-disable-line
-import './partial/init-heighttransition';
-import './partial/init-autoscroll';
-import 'flickity';
-import 'flickity-imagesloaded'<% };; %>
+import Vue from 'vue'
+import { sync } from 'vuex-router-sync'
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import router from './router/router'
+import store from './store/store'<% } %><% if (projectcraftbp === true || projectwordpressbp === true) { %>
+import progressLoader from './partial/progressloader'
+import initPhotoSwipeFromDOM from './partial/init-photoswipe' // eslint-disable-line
+import './partial/init-heighttransition'
+import './partial/init-autoscroll'
+import 'flickity'
+import 'flickity-imagesloaded'<% } %>
 import lazySizes from 'lazysizes'
 import lazybgset from 'lazysizes/plugins/bgset/ls.bgset'
 import './partial/kittnad' // Small Advertising for Kittn :)
 import './partial/modernizer-loader'
 import './partial/conditionizr-init'
 import './partial/disable-pointerevents'<% if ( projectJSFramework === 'Vue.js') { %>
-import App; from; './app.vue';
+import App from './app.vue'
 
-require('es6-promise').polyfill();
+require('es6-promise').polyfill()
 
 // keep vue-router and vuex store in sync
-sync(store, router);
+sync(store, router)
 
 // Adding Vue Plugins
-Vue.use(VueAxios, axios);
+Vue.use(VueAxios, axios)
 
 // Vue App
 /* eslint-disable no-new */
@@ -39,15 +39,15 @@ new Vue({
 })<% } %>
 
 // Lasysizes Lazyload Config
-window.lazySizesConfig = window.lazySizesConfig || {};
-window.lazySizesConfig.expand = 130;
-lazySizesConfig.expFactor = 1.3;
+window.lazySizesConfig = window.lazySizesConfig || {}
+window.lazySizesConfig.expand = 130
+lazySizesConfig.expFactor = 1.3
 
 // Lazy Sizes Init
-lazySizes.init()<%; if (projectcraftbp === true || projectwordpressbp === true) { %>
+lazySizes.init()<% if (projectcraftbp === true || projectwordpressbp === true) { %>
 
 // Init Photoswipe
-initPhotoSwipeFromDOM('.is-photoswipped');
+initPhotoSwipeFromDOM('.is-photoswipped')
 
 // Activate Progressloader
 progressLoader()<% } %>
