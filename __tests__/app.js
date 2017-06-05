49e2bb38230aca87153d9c0e97e32db1e7233bdb
Lars Eichler <larseichler.le@gmail.com>
2017-06-05 16:59:37 +0200

    New Test, let's see if it works

--- __tests__/app.js
+++ __tests__/app.js
@@ -1,17 +1,17 @@
-'use strict';
-var path = require('path');
-var assert = require('yeoman-assert');
-var helpers = require('yeoman-test');
+'use strict'
+var path = require('path')
+var assert = require('yeoman-assert')
+var helpers = require('yeoman-test')
 
 describe('generator-kittn:app', () => {
   beforeAll(() => {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true});
-  });
+      .withPrompts({someAnswer: true})
+  })
 
-  // It('creates files', () => {
-  //   assert.file([
-  //     'dummyfile.txt'
-  //   ]);
-  // });
-});
+  it('creates a package.json', () => {
+    assert.file([
+      'package.json'
+    ])
+  })
+})
