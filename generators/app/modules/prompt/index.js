c218fc766ba4e444fd8e9ca29f9af434d6ca0d2f
Lars Eichler <lars.eichler@1000grad.de>
2017-06-07 09:37:07 +0200

    Add Script-Prompts

--- generators/app/modules/prompt/index.js
+++ generators/app/modules/prompt/index.js
@@ -1,12 +1,14 @@
 const general = require('./partials/_general')
 const style = require('./partials/_style')
 const projecttype = require('./partials/_projecttype')
+const script = require('./partials/_script')
 
 function prompts () {
   return [
     ...general,
     ...style,
-    ...projecttype
+    ...projecttype,
+    ...script
   ]
 }
 
