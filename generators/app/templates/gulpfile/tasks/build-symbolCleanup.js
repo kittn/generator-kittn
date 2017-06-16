ab3d9089688626e1bd7da7e1e90a330d5a3a90bc
Lars Eichler <lars.eichler@1000grad.de>
2017-06-16 14:29:21 +0200

    Clean Up copied svgfiles as well

--- generators/app/templates/gulpfile/tasks/build-symbolCleanup.js
+++ generators/app/templates/gulpfile/tasks/build-symbolCleanup.js
@@ -11,16 +11,16 @@ const $ = gulpLoadPlugins()
 
 const buildSymbolCleanupTask = () => {
   return gulp
-    .src(kc.dist.cssimg + kc.src.images.vectorSprite.symbolName)
+    .src([kc.dist.cssimg + kc.src.images.vectorSprite.symbolName, kc.dist.vectors + '**'], { base: './' })
     .pipe($.cheerio({
       run: function ($) {
-        $('[fill^="#"]').removeAttr('fill');
-        $('[fill^="none"]').removeAttr('fill');
-        $('[fill-rule]').removeAttr('fill-rule');
+        $('[fill^="#"]').removeAttr('fill')
+        $('[fill^="none"]').removeAttr('fill')
+        $('[fill-rule]').removeAttr('fill-rule')
       },
       parserOptions: { xmlMode: true }
     }))
-    .pipe(gulp.dest(kc.dist.cssimg))
+    .pipe(gulp.dest('./'))
 }
 
 gulp.task('build:symbolCleanup', buildSymbolCleanupTask)
