42f4f7f5c71f180250915c8103a1ac1d63b6b64e
Sascha Fuchs <gisu2onic@gmail.com>
2017-06-21 01:44:13 +0200

    Adding moduletemplates

---
+++ generators/module/templates/_module.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * <%= moduleName %>
+ * =================
+ * Description
+ *
+ * @param $c = Classname
+ */
+
+// Module Vars
+$c = '<%= moduleType %>-<%= moduleName %>';
+?>
+
+<?php // Build Element Block ?>
+<div class="<?= $c ?>">
+
+</div>
