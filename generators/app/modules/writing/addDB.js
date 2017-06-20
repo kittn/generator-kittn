8aec033447fd2ad3929f732661cc602d74b32f49
Lars Eichler <lars.eichler@1000grad.de>
2017-06-20 16:57:19 +0200

    Fileright-Fix and mysql-reject-resolve

--- generators/app/modules/writing/addDB.js
+++ generators/app/modules/writing/addDB.js
@@ -21,17 +21,23 @@ const addDB = () => {
           connection.connect((err) => {
             if (err) {
               context.log('error connecting: ' + err.stack)
+              reject(err)
             }
           })
 
           connection.query('CREATE DATABASE IF NOT EXISTS ' + context.props.credentialdbdatabase + ';', (error, results, fields) => {
-            if (error) throw error
-            context.log('Added Database [' + context.props.credentialdbdatabase + '] to your MySQL DB')
+            if (error) {
+              reject(error)
+            } else {
+              context.log('Added Database [' + context.props.credentialdbdatabase + '] to your MySQL DB')
+            }
           })
           connection.end()
+          resolve()
         } else {
           if (context.props.projectcredential && context.props.credentialdbopen && context.commands.mysql !== true) {
             console.error('`mysql`-command not available')
+            reject()
           }
         }
       })
