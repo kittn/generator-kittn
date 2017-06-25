efb3074b007ace81f87ec4e906401fb2f801062d
Sascha Fuchs <gisu2onic@gmail.com>
2017-06-26 00:21:03 +0200

    Typofix for offCanvas Script

--- generators/app/templates/src/js/partial/offcanvas-trigger.js
+++ generators/app/templates/src/js/partial/offcanvas-trigger.js
@@ -9,31 +9,31 @@
 
 const offCanvasToggle = (open, close) => {
 
-  const $buttonClose = document.getElementsByClassName(close)[0]
-  const $buttonOpen = document.getElementsByClassName(open)[0]
-  const $root =  document.getElementsByTagName('body')[0]
+  const buttonClose = document.getElementsByClassName(close)[0]
+  const buttonOpen = document.getElementsByClassName(open)[0]
+  const root =  document.getElementsByTagName('body')[0]
 
 
   // Close Event
-  $buttonClose.addEventListener('click', function(event) {
+  buttonClose.addEventListener('click', function(event) {
     // Prevent Default Clicks
     event.preventDefault()
 
     // Toggle the ToggleClass
-    $root.classList.remove('is-offcanvas')
-    $buttonClose.classList.remove('is-offcanvas')
-    $buttonOpen.classList.remove('is-offcanvas')
+    root.classList.remove('is-offcanvas')
+    buttonClose.classList.remove('is-offcanvas')
+    buttonOpen.classList.remove('is-offcanvas')
   });
 
   // Open Event
-  $buttonOpen.addEventListener('click', function(event) {
+  buttonOpen.addEventListener('click', function(event) {
     // Prevent Default Clicks
     event.preventDefault()
 
     // Toggle the ToggleClass
-    $root.classList.add('is-offcanvas')
-    $buttonClose.classList.add('is-offcanvas')
-    $buttonOpen.classList.add('is-offcanvas')
+    root.classList.add('is-offcanvas')
+    buttonClose.classList.add('is-offcanvas')
+    buttonOpen.classList.add('is-offcanvas')
   })
 }
 
