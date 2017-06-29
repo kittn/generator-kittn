c00f552738ab34c62402df3c1f8448227671c0ba
Sascha Fuchs <gisu2onic@gmail.com>
2017-06-29 13:40:50 +0200

    Added JS Collection for Contentbuilder

---
+++ generators/app/templates/src/skeletons/starterpack/general/js/contentBuilder.js
@@ -0,0 +1,25 @@
+/**
+ * Essential JS for ContentBuilder
+ */
+import cq from 'cq-prolyfill'
+import progressLoader from './progressLoader'
+import initPhotoSwipeFromDOM from './init-photoswipe' // eslint-disable-line
+import disqusLoader from './disqusComments'
+import offCanvasToggle from './offcanvas-trigger'
+import './init-heighttransition'
+import './init-autoscroll'
+import './flickity-package'
+
+// Init Photoswipe
+initPhotoSwipeFromDOM('.is-photoswipped')
+
+// Activate Progressloader
+progressLoader()
+
+// Activate Disqus Button
+disqusLoader('.c-disqusComments__button')
+
+// Add Offcanvas Trigger
+offCanvasToggle('.c-offCanvas__trigger')
+
+cq({ postcss: true })
