eb520381a029acfc5927c4ae6298a0d15f55a732
Sascha Fuchs <gisu2onic@gmail.com>
2017-07-22 02:25:00 +0200

    Adding MySQL Host to the install.sh script

--- generators/app/templates/install.sh
+++ generators/app/templates/install.sh
@@ -2,7 +2,7 @@
 
 npm run init<% if (projectusage == "craft" || projectusage == "craftCB") { %>
 ./craftscripts/set_perms.sh<% } %>
-mysql -u<%= credentialdbuser %> -p<%= credentialdbpass %> <%= credentialdbdatabase %> < database.sql
+mysql -u<%= credentialdbuser %> -p<%= credentialdbpass %> -h<%= credentialdbserver %> <%= credentialdbdatabase %> < database.sql
 clear
 echo "Define vhost http://<%= credentialdomain %>"
 echo "Open Backend with you Browser http://<%= credentialdomain %>/<% if (projectusage == "wordpress" || projectusage == "wordpressCB") { %>wp-<% } %>admin"
