fbbb6e81a10370e96f328d77f69ef92e1b9fe734
Sascha Fuchs <gisu2onic@gmail.com>
2017-07-28 17:42:18 +0200

    Add resize for the hasScrollbar script

--- generators/app/templates/src/skeletons/starterpack/general/js/hasScrollbars.js
+++ generators/app/templates/src/skeletons/starterpack/general/js/hasScrollbars.js
@@ -2,11 +2,17 @@
  * Check if Browser have scrollbars, in this case the width from the Scrollbar is written as CSS Var in the body tag
  * @type {Element}
  */
-const body = document.querySelector('body')
+function handleWindow () {
+  const body = document.querySelector('body')
 
-if (window.innerWidth > body.clientWidth + 5) {
-  body.classList.add('has-scrollbar')
-  body.setAttribute('style', '--scroll-bar: ' + (window.innerWidth - body.clientWidth) + 'px')
-} else {
-  body.classList.remove('has-scrollbar')
+  if (window.innerWidth > body.clientWidth + 5) {
+    body.classList.add('has-scrollbar')
+    body.setAttribute('style', '--scroll-bar: ' + (window.innerWidth - body.clientWidth) + 'px')
+  } else {
+    body.classList.remove('has-scrollbar')
+  }
 }
+
+handleWindow()
+
+window.addEventListener('resize', handleWindow)
