a9436fbcd1dfc67e9e4ad4f9520391680c1c3902
Niels Nübel <n.nuebel@nn-medienagentur.de>
2017-12-06 12:28:46 +0100

    Fix eslint waring

--- generators/app/templates/src/skeletons/starterpack/general/js/searchbar.js
+++ generators/app/templates/src/skeletons/starterpack/general/js/searchbar.js
@@ -1,12 +1,12 @@
 const trigger = document.querySelector('.c-searchbar__trigger')
 const searchbar = document.querySelector('.c-searchbar')
 
-trigger.addEventListener('click', function () {
+trigger.addEventListener('click', () => {
   searchbar.classList.toggle('is-open')
   trigger.classList.toggle('is-open')
 })
 // Remove State on Keypress ESC
-document.onkeydown = function () {
+document.onkeydown = () => {
   if (event.keyCode === 27) {
     searchbar.classList.remove('is-open')
     trigger.classList.remove('is-open')
