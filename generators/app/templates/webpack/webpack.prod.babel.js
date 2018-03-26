fcdc93d669983b606fbf74671931ba2ca3e06b8f
Lars Eichler <lars.eichler@1000grad.de>
2018-03-26 12:55:34 +0200

    Fix Chunkpath for production-build

--- generators/app/templates/webpack/webpack.prod.babel.js
+++ generators/app/templates/webpack/webpack.prod.babel.js
@@ -15,6 +15,7 @@ const baseWebpackConfig = require('./webpack.config.base.babel.js')
 const prodWebpackConfig = merge(baseWebpackConfig.default, {
   output: {
     filename: utils.assetsPath('js/[name].<% if ( projectusage === 'webpackApp' ) { %>[hash].<% } %>js'),
+    chunkFilename: utils.assetsPath('js/chunks/[name].[hash].js'),
     publicPath: './'
   },
   plugins: [<% if ( projectusage === 'webpackApp' ) { %>
