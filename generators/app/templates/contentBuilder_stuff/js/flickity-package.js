bee8c368f6926691536f9ffa57f5f8ce8622f186
Sascha Fuchs <gisu2onic@gmail.com>
2018-04-29 00:37:12 +0200

    Extended Flickity Config, with Default Vars. Prepared for Extend

--- generators/app/templates/contentBuilder_stuff/js/flickity-package.js
+++ generators/app/templates/contentBuilder_stuff/js/flickity-package.js
@@ -3,10 +3,17 @@
  * @param element (string) =
  */
 
-import flickity from 'flickity'
+import Flickity from 'flickity'
 import 'flickity-imagesloaded'
 
 const flickityPackage = () => {
+  const flickityDefaults = {
+    accessibility: false,
+    contain: true,
+    imagesLoaded: true,
+    lazyLoad: false
+  }
+
   const slider = document.querySelector('.js-imageSlider')
 
   if (slider !== null) {
@@ -15,7 +22,25 @@ const flickityPackage = () => {
         const sliders = Array.from(document.querySelectorAll('.js-imageSlider'))
         if (sliders) {
           sliders.forEach((slider) => {
-            const flkty = flickity.data(slider)
+            // Check if outer transport Wrapper is used
+            let flkty
+
+            // If Slidertransport is not used as Wrapper yield data from the main slider wrapper
+            const sliderTransport = slider.querySelector('.flickity-custom__transport') || slider
+
+            // Check if the right Data Attr is available, otherwise use an empty object
+            const preCustomData =
+              sliderTransport.hasAttribute('data-myflickity')
+                ? JSON.parse(sliderTransport.dataset.myflickity)
+                : {}
+
+            // Merging with Defaults
+            const customData = Object.assign(flickityDefaults, preCustomData)
+
+            // Init Flickity
+            flkty = new Flickity(sliderTransport, customData)
+
+            // Fix Height
             flkty.resize()
             slider.classList.add('is-visible')
           })
