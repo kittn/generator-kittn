7dfd51d4f3e76eb61c1076af7a261f24c1afa0bd
Sascha Fuchs <gisu2onic@gmail.com>
2018-04-28 23:37:21 +0200

    Moved Lazysize Stuff in a seperate Module

--- generators/app/templates/src/js/main.js
+++ generators/app/templates/src/js/main.js
@@ -4,13 +4,12 @@ import React from 'react'
 import ReactDOM from 'react-dom'<% } %><% if ((typeof projectcontainerqueries !== 'undefined' && projectcontainerqueries === true) || (typeof projectusage !== 'undefined' && projectusage === 'craftCB') || (typeof projectusage !== 'undefined' && projectusage === 'joomlaCB') || (typeof projectusage !== 'undefined' && projectusage === 'wordpressCB'))  { %>
 import cq from 'cq-prolyfill' // eslint-disable-line<% } %><% if ((typeof projectusage !== 'undefined' && projectusage === 'craftCB') || (typeof projectusage !== 'undefined' && projectusage === 'joomlaCB') || (typeof projectusage !== 'undefined' && projectusage === 'wordpressCB')) { %>
 import contentBuilder from './partial/contentBuilder'<% } %><% if ( projectusage !== 'webpackApp' ) { %>
-import lazySizes from 'lazysizes'
-import lazybgset from 'lazysizes/plugins/bgset/ls.bgset' // eslint-disable-line<% } %>
+import lazySizeInit from './partial/lazysize-init'<% } %>
 import 'svgxuse' // eslint-disable-line
 import './partial/kittnad' // Small Advertising for Kittn :)
 import './partial/detect-browser'
 import './partial/disable-pointerevents'<% if ( projectusage !== 'webpackApp' ) { %>
-if (window.HTMLPictureElement) {
+if (!window.HTMLPictureElement) {
   import('lazysizes/plugins/respimg/ls.respimg.js' /* webpackChunkName: "picturePolyfill" */)
 }<%}%><% if ( typeof projectjsframework !== 'undefined' && projectjsframework === 'react') { %>
 ReactDOM.render(
@@ -22,14 +21,8 @@ if (document.querySelectorAll('.js-vue').length > 0) {
   import('./vue-app' /* webpackChunkName: "vueapp" */)
 }<% } %><% if ( projectusage !== 'webpackApp' ) { %>
 
-// Lasysizes Lazyload Config
-const lazySizesConfig = window.lazySizesConfig || {}
-window.lazySizesConfig = lazySizesConfig
-window.lazySizesConfig.expand = 130
-lazySizesConfig.expFactor = 1.3
-
-// Lazy Sizes Init
-lazySizes.init()<% } %><% if ((typeof projectcontainerqueries !== 'undefined' && projectcontainerqueries === true) || (typeof projectusage !== 'undefined' && projectusage === 'craftCB') || (typeof projectusage !== 'undefined' && projectusage === 'joomlaCB') || (typeof projectusage !== 'undefined' && projectusage === 'wordpressCB') ) { %>
+// Init Lazysizes
+lazySizeInit()<% } %><% if ((typeof projectcontainerqueries !== 'undefined' && projectcontainerqueries === true) || (typeof projectusage !== 'undefined' && projectusage === 'craftCB') || (typeof projectusage !== 'undefined' && projectusage === 'joomlaCB') || (typeof projectusage !== 'undefined' && projectusage === 'wordpressCB') ) { %>
 
 // Activate Container Queries
 cq({ postcss: true })<% } %><% if (projectusage === 'webpackApp' || projectjsframework === 'vue') { %>
